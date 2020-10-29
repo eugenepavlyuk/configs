4ea0d21e003d7ce3dbd61449463e9a3ff4bced7c
Ievgen Pavliuk <pavliuki@dev-dsk-pavliuki-1b-0664a962.eu-west-1.amazon.com>
2020-10-29 07:43:19 +0000

    Updated configs

--- zsh_config.sh
+++ zsh_config.sh
@@ -3,7 +3,38 @@ alias sublime="rsub"
 
 alias config="sublime $HOME/.zsh_config.sh"
 alias profile="sublime $HOME/.zshrc"
+alias onboarding="sublime $HOME/playground/onboarding/onboarding_cloud.sh"
+alias zconfig="sublime $HOME/playground/configs/zsh_config.sh"
 
 reload() {
 	source $HOME/.zshrc
 }
+
+pull_onboarding() {
+	pushd "$HOME/playground/onboarding/"
+	git pull origin master
+	popd
+}
+
+push_onboarding() {
+	pushd "$HOME/playground/onboarding/"
+	git add *
+	git commit -m "Updated onboarding"
+	git push origin master
+	popd
+}
+
+pull_config() {
+	pushd "$HOME/playground/configs/"
+	git pull origin master
+	popd
+}
+
+push_configs() {
+	pushd "$HOME/playground/configs/"
+	git add *
+	git commit -m "Updated configs"
+	git push origin master
+	popd
+}
+
